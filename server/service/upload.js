df4a96c6bd8e62f06c126e90aaa22dfe7f8e755a
Theycallmeki <laxamana.kian13@gmail.com>
2026-01-23 14:49:42 +0800

    fix: import in upload.js & helper.js

--- server/service/upload.js
+++ server/service/upload.js
@@ -1,18 +1,11 @@
 const multer = require("multer");
 const multerS3 = require("multer-s3");
-const AWS = require("aws-sdk");
-
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
-});
+const { s3 } = require("../config/s3");   
 
 const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: process.env.AWS_BUCKET_NAME,
-
 
     contentType: multerS3.AUTO_CONTENT_TYPE,
 
@@ -30,7 +23,6 @@ const upload = multer({
     cb(null, true);
   },
 });
-
 
 const handleMulterError = (err, req, res, next) => {
   if (err instanceof multer.MulterError) {
